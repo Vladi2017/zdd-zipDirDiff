f5a9e616b015e0c167d75553427572903900c550
Vladi Manolescu <mvmanol@yahoo.com>
2017-07-24 12:45:02 +0300

    List directories using 2 methods:
    One through opendir and readdir (it seems not recursively)
    2nd through external command.. whose output can be captured in a Perl variable for
    further processing..

--- V4.pl
+++ V4.pl
@@ -5,7 +5,16 @@ use warnings;
 use Archive::Zip qw( :ERROR_CODES :CONSTANTS );
 #read a Zip file
 my $zipFile1 = Archive::Zip->new();
-unless ( $zipFile1->read( shift(@ARGV) ) == AZ_OK ) { die 'read error Vladi';}
+my $arg1 = shift(@ARGV);
+unless ( $zipFile1->read($arg1) == AZ_OK ) { die 'read error Vladi';}
 my @list1 = $zipFile1->memberNames();
 print $_ . "\n" foreach(@list1);
+$arg1 =~ /(^\W*\w+)/; #Vl.tthe directory name must not have non-word character
+print $1 . "\n";
+opendir(my $dh, $1) || die 'Vl.opendir error';
+while(readdir $dh) {print "$1/$_\n";}
+closedir $dh;
+#Vl.recursive dir in a perl var as output of an external command using backticks (``)
+my $dir1 = `ls -R`;
+print $dir1;
 
