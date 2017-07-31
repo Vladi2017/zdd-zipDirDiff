bc5768f4eb48cd3bb1bb3617dabc22f2a297968f
Vladi Manolescu <mvmanol@yahoo.com>
2017-07-31 19:01:00 +0300

    VkeyWord, Vtopic: How do I find the index of a specific array value? pi@raspberrypi:~/projects1/compare1 $ ./t1.pl index of Toes = 3 pi@raspberrypi:~/projects1/compare1 $

--- t1.pl
+++ t1.pl
@@ -11,6 +11,7 @@ print "number of matches of \/ is: " . scalar @_ . "\n";
 # Vl.be aware that $_ gets assigment from STDIN only in while like constructs..
 =cut
 
+=test2
 # perldoc -q array list
 # Found in /usr/lib/perl5/5.22/pods/perlfaq4.pod; How can I remove duplicate elements from a list or array?
 my @array = qw/unu doi trei unu cinci patru trei sapte trei noua sapte opt trei doi/; 
@@ -28,4 +29,12 @@ print "keys %seen: @keys1\n";
 my @values1 = values %seen;
 print "vales %seen: @values1\n";
 print "unique array/list : @unique\n";
+=cut
+
+use List::Util qw(first);
+my @array = qw( Apples Oranges Brains Toes Kiwi);
+my $search = "Toes";
+my $index = first { $array[$_] eq $search } 0 .. $#array; ##this is OK. returns the searched index
+## my $index = first { $array[$_] eq $search } @array; Vl.this is not ok.. Argument "Apples" isn't numeric in array element at ./t1.pl line 38.
+print "index of $search = $index\n";
 
